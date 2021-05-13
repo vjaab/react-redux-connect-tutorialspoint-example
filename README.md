542420672b5bad38d94a4dc56665b8f265a9e20d
vjaab <vijayakumarj.official@gmail.com>
2021-05-13 09:52:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-klx4w7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-klx4w7)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Counter from '../src/container/counterContainer';
+
+class App extends Component {
+   render() {
+      return (
+         <div className = "App">
+            <header className = "App-header">
+               <Counter/>
+            </header>
+         </div>
+      );
+   }
+}
+export default App;
